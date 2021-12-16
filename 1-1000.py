5b7aaab670954cbf7eed5c1730bcaca4172ac73a
ahaef123 <96227883+ahaef123@users.noreply.github.com>
2021-12-16 09:02:52 +0100

    Add files via upload

---
+++ 1-1000.py
@@ -0,0 +1,7 @@
+i = 1                       # definiere i
+
+while i <= 1000:            # wenn i kleiner oder gleich 1000 ist:
+    print(i)                # soll i angezeigt werden
+    i = i + 1               # nach der Ausgabe von i, wird i um 1 erhöht
+                            # Da es ein While-Loop ist, wird nach der Erhöhung von i wieder geprüft ob i kleiner oder gleich als 1000 ist. 
+                            # Sobald i 1000 ist, ist das Programm fertig
